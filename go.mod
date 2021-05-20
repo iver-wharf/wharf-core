189087f388a5f02639ff3ac584e81814c5d8efc9
Kalle Jillheden <kalle.jillheden@iver.se>
2021-05-20 16:07:47 +0200

    Package doc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iver-wharf/wharf-core
+
+go 1.16
---
+++ pkg/config/doc.go
@@ -0,0 +1,4 @@
+// Config package helps you with reading configuration from files and
+// environment variables in a unified way. This package is used throughout Wharf
+// to read configurations.
+package config
