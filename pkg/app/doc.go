7bd9a4f2824b4e18faff296d2e7f839673cca52c
Kalle Jillheden <kalle.jillheden@iver.se>
2021-05-21 10:24:17 +0200

    Fixed package doc format

--- pkg/app/doc.go
+++ pkg/app/doc.go
@@ -1,3 +1,3 @@
-// App package contains utility types and functions regarding metadata about the
+// Package app contains utility types and functions regarding metadata about the
 // application itself, such as versioning.
 package app
