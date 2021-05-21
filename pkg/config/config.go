8ba61d9b0710ef091f65a1d2865a220969130a32
Kalle Jillheden <kalle.jillheden@iver.se>
2021-05-21 11:07:38 +0200

    It works

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,24 +1,38 @@
 package config
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"strings"
 
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
+
+const configTypeYAML = "yaml"
 
 type Config interface {
 	AddConfigYAMLFile(path string)
 	AddConfigYAML(reader io.Reader)
 	AddEnvironmentVariables(prefix string)
+	Unmarshal(config interface{}) error
 }
 
-func New() Config {
-	return &config{}
+func New(defaultConfig interface{}) Config {
+	return &config{
+		defaultConfig: defaultConfig,
+	}
 }
 
 type config struct {
-	sources []configSource
+	defaultConfig interface{}
+	sources       []configSource
+}
+
+type configSource interface {
+	name() string
+	apply(v *viper.Viper) error
 }
 
 func (c *config) AddConfigYAMLFile(path string) {
@@ -33,32 +47,77 @@ func (c *config) AddEnvironmentVariables(prefix string) {
 	c.sources = append(c.sources, envVarsSource{prefix})
 }
 
-type configSource interface {
-	apply(viper viper.Viper)
+func (c *config) Unmarshal(config interface{}) error {
+	v := viper.New()
+	initDefaults(v, c.defaultConfig)
+	for _, s := range c.sources {
+		if err := s.apply(v); err != nil {
+			return fmt.Errorf("applying config source: %s: %w", s.name(), err)
+		}
+	}
+	return v.Unmarshal(config)
+}
+
+func initDefaults(v *viper.Viper, defaultConfig interface{}) error {
+	// Uses a workaround to force viper to read environment variables
+	// by making it aware of all fields that exists so it can later map
+	// environment variables correctly.
+	// https://github.com/spf13/viper/issues/188#issuecomment-413368673
+	b, err := yaml.Marshal(defaultConfig)
+	if err != nil {
+		return fmt.Errorf("setting config defaults: %w", err)
+	}
+	defaultCfg := bytes.NewReader(b)
+	v.SetConfigType(configTypeYAML)
+	if err := v.MergeConfig(defaultCfg); err != nil {
+		return fmt.Errorf("setting config defaults: %w", err)
+	}
+	return nil
 }
 
 type yamlFileSource struct {
 	path string
 }
 
-func (c yamlFileSource) apply(viper viper.Viper) {
-	viper.AddConfigPath(c.path)
+func (s yamlFileSource) name() string {
+	return s.path
+}
+
+func (s yamlFileSource) apply(v *viper.Viper) error {
+	if s.path == "" {
+		// viper does not set config file if its empty, so viper.MergeInConfig()
+		// would then use the previously set config path value
+		return nil
+	}
+	v.SetConfigType(configTypeYAML)
+	v.SetConfigFile(s.path)
+	return v.MergeInConfig()
 }
 
 type yamlSource struct {
 	reader io.Reader
 }
 
-func (c yamlSource) apply(viper viper.Viper) {
-	viper.ReadConfig(c.reader)
+func (s yamlSource) name() string {
+	return "YAML io.Reader"
+}
+
+func (s yamlSource) apply(v *viper.Viper) error {
+	v.SetConfigType(configTypeYAML)
+	return v.MergeConfig(s.reader)
 }
 
 type envVarsSource struct {
 	prefix string
 }
 
-func (c envVarsSource) apply(viper viper.Viper) {
-	viper.SetEnvPrefix(c.prefix)
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+func (s envVarsSource) name() string {
+	return "environment variables"
+}
+
+func (s envVarsSource) apply(v *viper.Viper) error {
+	v.SetEnvPrefix(s.prefix)
+	v.AutomaticEnv()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	return nil
 }
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,15 +1,11 @@
 package config_test
 
 import (
-	"bytes"
-	"errors"
-	"io/fs"
 	"os"
 	"strings"
 	"testing"
 
-	"github.com/spf13/viper"
-	"gopkg.in/yaml.v2"
+	"github.com/iver-wharf/wharf-core/pkg/config"
 )
 
 type InnerConfig struct {
@@ -22,11 +18,13 @@ type RootConfig struct {
 }
 
 func DefaultConfig() RootConfig {
-	return RootConfig{}
+	return RootConfig{
+		Daz: "default daz",
+	}
 }
 
 var sampleConfYaml = `
-daz: conf daz
+#daz: conf daz
 moo:
   foo: conf foo
 `
@@ -34,45 +32,21 @@ moo:
 func TestEnvConfig(t *testing.T) {
 	var cfg RootConfig
 
+	c := config.New(DefaultConfig())
+
+	c.AddConfigYAML(strings.NewReader(sampleConfYaml))
+	c.AddConfigYAMLFile("wharf-config.yml")
+	c.AddEnvironmentVariables("")
+
 	os.Setenv("MOO_FOO", "moo foo")
-	//os.Setenv("DAZ", "env daz")
 
-	// May be ugly, but it works
-	// https://github.com/spf13/viper/issues/188#issuecomment-413368673
-	b, err := yaml.Marshal(DefaultConfig())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("\n%s", string(b))
-
-	defaultCfg := bytes.NewReader(b)
-	viper.SetConfigType("yaml")
-	if err := viper.MergeConfig(defaultCfg); err != nil {
-		t.Fatal(err)
-	}
-
-	viper.SetConfigFile("/etc/my-app")
-	if err := viper.MergeInConfig(); err != nil {
-		if !errors.Is(err, fs.ErrNotExist) {
-			t.Fatalf("%T: %v", err, err)
-		}
-	}
-
-	reader := strings.NewReader(sampleConfYaml)
-	if err := viper.MergeConfig(reader); err != nil {
-		t.Fatalf("%T: %v", err, err)
-	}
-
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	if err := viper.Unmarshal(&cfg); err != nil {
+	if err := c.Unmarshal(&cfg); err != nil {
 		t.Fatal("unmarshal config:", err)
 	}
 
 	if cfg.Moo.Foo != "moo foo" {
 		t.Errorf("wanted MOO_FOO to be 'moo foo', got: %+v", cfg)
 	}
+
 	t.Errorf("%+v", cfg)
 }
