6d73ca0c82151aaf78494f4b73fdaaa5cbc971ad
Kalle Jillheden <kalle.jillheden@iver.se>
2021-07-05 11:18:42 +0200

    Refactored strutil a little

--- pkg/strutil/strutil.go
+++ pkg/strutil/strutil.go
@@ -9,31 +9,23 @@ import (
 // FirstRuneLower returns the same string but with the first rune in the string
 // transformed to lowercase.
 func FirstRuneLower(value string) string {
-	if len(value) == 0 {
-		return value
-	}
 	first, size := utf8.DecodeRuneInString(value)
-	if first == utf8.RuneError {
+	if first == utf8.RuneError || unicode.IsLower(first) {
 		return value
 	}
-	if unicode.IsLower(first) {
-		return value
-	}
-	return fmt.Sprintf("%c%s", unicode.ToLower(first), value[size:])
+	return concatRuneString(unicode.ToLower(first), value[size:])
 }
 
 // FirstRuneUpper returns the same string but with the first rune in the string
 // transformed to uppercase.
 func FirstRuneUpper(value string) string {
-	if len(value) == 0 {
-		return value
-	}
 	first, size := utf8.DecodeRuneInString(value)
-	if first == utf8.RuneError {
+	if first == utf8.RuneError || unicode.IsUpper(first) {
 		return value
 	}
-	if unicode.IsUpper(first) {
-		return value
-	}
-	return fmt.Sprintf("%c%s", unicode.ToUpper(first), value[size:])
+	return concatRuneString(unicode.ToUpper(first), value[size:])
+}
+
+func concatRuneString(r rune, s string) string {
+	return fmt.Sprintf("%c%s", r, s)
 }
