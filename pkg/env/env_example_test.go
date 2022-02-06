4e49d726ef4e54b0f37ac6bf698dcba9196775b9
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-06 11:55:03 +0100

    Changed env.Bind to use generic instead of reflect for type assertion

--- pkg/env/env_example_test.go
+++ pkg/env/env_example_test.go
@@ -24,11 +24,6 @@ func ExampleBind() {
 	fmt.Printf("Before: A: %q\n", x.A)
 	fmt.Printf("Before: B: %d\n", x.B)
 
-	err := env.Bind(x.A, "A")
-	fmt.Printf("Parse A: %s (is ErrNotAPointer? %t)\n", err, errors.Is(err, env.ErrNotAPointer))
-	err = env.Bind(&x, "A")
-	fmt.Printf("Parse A: %s (is ErrUnsupportedType? %t)\n", err, errors.Is(err, env.ErrUnsupportedType))
-
 	env.Bind(&x.A, "A")
 	env.Bind(&x.B, "B")
 
@@ -36,14 +31,12 @@ func ExampleBind() {
 	fmt.Printf("After: B: %d\n", x.B)
 
 	os.Setenv("C", "foo bar")
-	err = env.Bind(&x.C, "C")
+	err := env.Bind(&x.C, "C")
 	fmt.Printf("Parse C: %s (is ErrParse? %t)\n", err, errors.Is(err, env.ErrParse))
 
 	// Output:
 	// Before: A: ""
 	// Before: B: 0
-	// Parse A: env "A": unsupported type: not a pointer: string (is ErrNotAPointer? true)
-	// Parse A: env "A": unsupported type: *env_test.testType (is ErrUnsupportedType? true)
 	// After: A: "1"
 	// After: B: 2
 	// Parse C: env "C"="foo bar": time: invalid duration "foo bar" (is ErrParse? true)
--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testBind(t *testing.T, ptr any, envKey string, envValue string, want any) {
+func testBind[T BindConstraint](t *testing.T, ptr T, envKey string, envValue string, want any) {
 	t.Run(envKey, func(t *testing.T) {
 		testutil.SetEnv(t, envKey, envValue)
 		require.NoError(t, Bind(ptr, envKey))
@@ -49,5 +49,5 @@ func TestBind(t *testing.T) {
 }
 
 func TestBindMultiple_noErrorOnNilMap(t *testing.T) {
-	assert.NoError(t, BindMultiple(nil))
+	assert.NoError(t, BindMultiple[*int](nil))
 }
