5fe3452eb20d63f4e542a3356823d0eee8027c75
kalle (jag) <kalle.fagerberg@iver.se>
2022-03-30 10:03:58 +0200

    Fixed scope logging (#45)
    * Added test
    
    * Updated CHANGELOG.md

--- pkg/logger/consolepretty/pretty_test.go
+++ pkg/logger/consolepretty/pretty_test.go
@@ -1,10 +1,13 @@
 package consolepretty
 
 import (
+	"bytes"
 	"fmt"
 	"math"
 	"testing"
 
+	"github.com/fatih/color"
+	"github.com/iver-wharf/wharf-core/v2/pkg/logger"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -37,6 +40,65 @@ func TestPrintedIntLen(t *testing.T) {
 			assert.Equal(t, tc.want, printedIntLenFast(tc.input), "printedIntLenFast")
 		})
 	}
+}
+
+func TestContextWriteScope(t *testing.T) {
+	tests := []struct {
+		name    string
+		scope   string
+		config  Config
+		longest int
+		want    string
+	}{
+		{
+			name:    "no scope",
+			scope:   "",
+			longest: 0,
+			want:    "",
+		},
+		{
+			name:    "with scope",
+			scope:   "abc",
+			longest: 0,
+			want:    "|abc",
+		},
+		{
+			name:    "padded",
+			scope:   "abc",
+			config:  Config{ScopeMinLength: 6},
+			longest: 0,
+			want:    "|abc   ",
+		},
+		{
+			name:    "autopadded",
+			scope:   "abc",
+			config:  Config{ScopeMinLengthAuto: true},
+			longest: 6,
+			want:    "|abc   ",
+		},
+		{
+			name:    "maxxed",
+			scope:   "abcdef",
+			config:  Config{ScopeMaxLength: 3},
+			longest: 0,
+			want:    "|abc",
+		},
+	}
+	color.NoColor = true
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			logger.LongestScopeNameLength = tc.longest
+			tc.config.Coloring = &DefaultColorConfig
+			ctx := context{
+				scope:  tc.scope,
+				Config: &tc.config,
+			}
+			var buf bytes.Buffer
+			ctx.writeScope(&buf)
+			assert.Equal(t, tc.want, buf.String())
+		})
+	}
+	logger.LongestScopeNameLength = 0
 }
 
 var varThatDisablesCompilerOptimizations int
